5e74996f51fdcdff178af7cfd060f90afea134f1
Rolando Guedes <rolandoguedes@gmail.com>
2020-09-23 12:52:05 +0100

    Update flutter_icons.dart

--- lib/templates/flutter_icons.dart
+++ lib/templates/flutter_icons.dart
@@ -16,10 +16,26 @@ class _%CLASS_NAME%Data extends IconData {
 class %CLASS_NAME% {
 %INDENT%%CLASS_NAME%._();
 
+%INDENT%static Map<String, dynamic> _toMap() {
+%INDENT%return {
+%INDENT%%INDENT%%MAP%
+%INDENT%}
+}
+
+static dynamic get(String propertyName) {
+    var _mapRep = _toMap();
+    if (_mapRep.containsKey(propertyName)) {
+        return _mapRep[propertyName];
+    }
+    throw ArgumentError('Icon not found');
+}
+
 %INDENT%
 %CONTENT%
 }
 ''';
+
+const String icon_map = "'%ICON_NAME%': %ICON_NAME%";
 
 const String icon = '%INDENT%static const IconData %ICON_NAME% = '
     '_%CLASS_NAME%Data(0x%ICON_CODE%);';
